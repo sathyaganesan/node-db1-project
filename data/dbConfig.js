1e4be3d59b5e17404383b68ff3696612c67c6888
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-12-16 23:31:13 -0500

    GET, PUT, POST requests are done

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,6 @@ const knex = require("knex");
 const knexfile = require("../knexfile.js");
 
 // change to "production" and update knexfile.js to use postgres.
-const database = "development";
+const production = "development";
 
-module.exports = knex(knexfile[database]);
+module.exports = knex(knexfile[production]);
