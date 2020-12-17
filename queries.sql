2f71870e67860c7b7fd7cadb833b47887e89dcc8
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-12-16 21:10:09 -0500

    SQL MVPs are done

--- queries.sql
+++ queries.sql
@@ -1,16 +1,40 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+    => Cactus Comidas para llevar
+    => Océano Atlántico Ltda.
+    => Rancho grande
 
 -- Find the phone number for the supplier with the id 11
+    => (010) 9984510
 
 -- List first 10 orders placed, sorted descending by the order date
+    => FOLIG
+    => VINET
+    => KOENE
+    => PICCO
+    => SUPRD
+    => VICTE
+    => MEREP
+    => GROSR
+    => BLONP
+    => ROMEY
 
 -- Find all customers that live in London, Madrid, or Brazil
-
+    => Around the Horn, London.
+    => Bólido Comidas preparadas, Madrid.
+    => B's Beverages, London.
+    => Consolidated Holdings, London
+    => Eastern Connection, London
+    => FISSA Fabrica Inter. Salchichas S.A, Madrid
+    => North/South, London
+    => Romero y tomillo, Madrid
+    => Seven Seas Imports, London
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+    => Done
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+    => Done
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
